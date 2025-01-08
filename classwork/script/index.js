55b03fd1ca9d5b145f8a5fb0e53338ba3bbc4d44
Artem <artem.99.zaikin@gmail.com>
2025-01-08 16:28:35 +0100

    hw6

--- classwork/script/index.js
+++ classwork/script/index.js
@@ -1,15 +1,31 @@
+function sortFunktion() {
 
+	const array = Array.from(arguments);
 
-//closure - замикання 
-function createCounter() {
-	let count = 0;
-	return function () {
-		count += 1;
-		return count;
+	if (array.length <= 1) {
+		return array;
 	};
-}
 
-const counter = createCounter();
-console.log(counter);
-console.log(createCounter());
+	const center = array[Math.floor(array.length / 2)];
+	const left = [];
+	const right = [];
+	const equal = [];
 
+	for (let num of array) {
+		if (num < center) {
+			left.push(num);
+		} else if (num > center) {
+			right.push(num);
+		} else {
+			equal.push(num);
+		}
+	};
+
+	return [...sortFunktion(...left), ...equal, ...sortFunktion(...right)];
+};
+
+const sortedArray = sortFunktion(5, 2, 9, 1, 5, 6, 1798, 22, 54, 0, 3);
+console.log(sortedArray);
+
+let word = "hello"
+console.log(word.split("").reverse().join(""))
